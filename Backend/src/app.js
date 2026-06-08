28290446160b90c878b21dfe1de3865418da9365
Bishal Das <dasbishal5089@gmail.com>
2026-06-08 17:01:03 +0530

    fix the cross erros after deploying

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,10 +6,26 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser())
-app.use(cors({
-    origin:"http://localhost:5174",
-    credentials:true
-}))
+// app.use(cors({
+//     origin:"http://localhost:5174",
+//     credentials:true
+// }))
+
+// app.use(cors({
+//     origin: [
+//     //   "http://localhost:5174",
+//       "https://full-stack-preap-ai-project.vercel.app/"
+//     ],
+//     credentials: true
+//   }));
+
+  app.use(cors({
+    origin: [
+      "http://localhost:5174",
+      "https://bishal-preap-ai.netlify.app"
+    ],
+    credentials: true
+  }));
 
 // required all the routes here
 const authRouter = require("./routes/auth.routes");
